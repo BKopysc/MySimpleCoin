f74628673cb6ac86e24c4d0c63d4ff2f0e9ab3f2
Bartlomiej Kopysc <bkopysc@wp.pl>
2023-11-21 16:41:32 +0100

    block testing

--- blockchain_block.py
+++ blockchain_block.py
@@ -3,30 +3,25 @@ import time
 from hashlib import sha256
 
 # Testowo, nie wiem czy dobrze
+# https://www.geeksforgeeks.org/implementing-the-proof-of-work-algorithm-in-python-for-blockchain-mining/
 
 class BlockchainBlock(object):
     # Constructor
-    def __init__(self, previous_hash:str = None, timestamp: float = None, index = 0, transactions: [] = []):
+    def __init__(self, previous_hash:str = None, transactions: [] = []):
         self.version = 1
-        self.index =1
-        self.nonce = 1
-        self.difficulty = 1
-        self.previous_hash = previous_hash
+        self.nonce = 0
+        #self.difficulty = 1
         self.transactions = transactions
-        if(timestamp == None):
-            self.timestamp = time.time()
-        else:
-            self.timestamp = timestamp
-
+        self.timestamp = time.time()
+        self.previous_hash = previous_hash
+        self.hash = self.__generate_hash()
     
     def get_block_as_dict(self):
-        return({"version": self.version, 
-                "nonce": self.nonce, 
-                "previous_hash": self.previous_hash, 
-                "timestamp": self.timestamp, 
-                "transactions": self.transactions})
+        # self to dict
+        return({ 'version': self.version, 'nonce': self.nonce, 'transactions': self.transactions,
+                 'timestamp': self.timestamp, 'previous_hash': self.previous_hash})
 
-    def get_hash(self):
+    def __generate_hash(self):
         # sha256 hash of the block dict
         encoded = sha256(str(self.get_block_as_dict()).encode('utf-8')).hexdigest()
         return(encoded)
